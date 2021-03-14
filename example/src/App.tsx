07ca96ac7f4ca3a1382c1beef4184af1fcaa8212
vivien le duc <leducvivien@gmail.com>
2021-03-14 15:37:16 +0100

    example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,21 +3,10 @@ import React from 'react'
 import { Text, Typo } from 'typo'
 
 const App = () => {
-
   return (
-    <Typo pause={500} pace={100} whiteSpacePace={500}
-      onStart={(typo) => console.log(typo + ' typo start')}
-      onText={(text, typo) => console.log(typo + ' text play ' + text)}
-      onStop={(typo) => console.log(typo + ' typo stop')}
-      onChar={(char, typo) => console.log(typo + ' typo char ' + char)}
-    >
-      <Text
-        charClassName={'wave'} className={'container'}
-      >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, suscipit temporibus animi voluptatem debitis laborum culpa dolorum. Quidem cumque corrupti animi in distinctio aperiam rerum perferendis maxime, dolore fugit nulla.</Text>
-
-      <Text
-        charClassName={'wave'} className={'container'}
-      >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus inventore tempora, commodi excepturi quas deserunt quia quidem quae molestiae maxime vitae ipsum suscipit cumque. Non nesciunt tenetur at provident doloremque.</Text>
+    <Typo>
+      <Text>Hello </Text>
+      <Text>World!</Text>
     </Typo>
   )
 }
