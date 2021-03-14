62a9a62c8d07131441c2db54a686071d7df64a58
vivien le duc <leducvivien@gmail.com>
2021-03-14 14:41:22 +0100

    test animation

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -5,18 +5,18 @@ import { Text, Typo } from 'typo'
 const App = () => {
 
   return (
-    <Typo pause={1000} pace={100} whiteSpacePace={500}
+    <Typo pause={500} pace={100} whiteSpacePace={500}
       onStart={(typo) => console.log(typo + ' typo start')}
       onText={(text, typo) => console.log(typo + ' text play ' + text)}
       onStop={(typo) => console.log(typo + ' typo stop')}
       onChar={(char, typo) => console.log(typo + ' typo char ' + char)}
     >
       <Text
-        charClassName={'rotate-in-center'} className={'container'}
+        charClassName={'wave'} className={'container'}
       >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, suscipit temporibus animi voluptatem debitis laborum culpa dolorum. Quidem cumque corrupti animi in distinctio aperiam rerum perferendis maxime, dolore fugit nulla.</Text>
 
       <Text
-        charClassName={'rotate-in-center'} className={'container'}
+        charClassName={'wave'} className={'container'}
       >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus inventore tempora, commodi excepturi quas deserunt quia quidem quae molestiae maxime vitae ipsum suscipit cumque. Non nesciunt tenetur at provident doloremque.</Text>
     </Typo>
   )
