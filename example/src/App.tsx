24402fdaa5cca8506ee126664f89eed6faf212b6
vivien le duc <leducvivien@gmail.com>
2021-03-15 01:12:48 +0100

    under construction

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -11,10 +11,12 @@ const App = () => {
         <Text pause={200} charClassName="wave-infinite" className="title title2" stamp>Reactypo</Text>
         <Text pause={500} whiteSpacePace={100} charClassName=" sub-title-char" className={'sub-title'}>animate and control text with react</Text>
       </Typo>
-
-      <Typo name="under-contruction" className="under-contruction" whiteSpacePace={150}>
-        <Text charClassName="slide-in-bck-center" stamp block>This documentation is under construction...</Text>
-    </Typo>
+      
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <Typo className="under-contruction" name="under-contruction">
+          <Text pause={200} charClassName="swirl-in-fwd" >This documentation is under construction</Text>
+          </Typo>
+      </div>
     </main>
   )
 }
